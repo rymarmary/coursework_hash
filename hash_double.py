31e74438f695f2202490b873496e3ba3d9ad59bf
rymarmary <rymarmary2003@gmail.com>
2022-12-23 02:27:03 +0300

    Add theory and create hash classes

---
+++ hash_double.py
@@ -0,0 +1,34 @@
+import math
+
+class DoubleHash:
+    def __init__(self, prime):
+        self.num = prime
+        self.get_prime()  # changing num
+        self.table = [[],] * self.num
+
+    def check_prime(self):  # check if num is prime
+        if self.num == 1 or self.num == 0:
+            return 0
+        for i in range(2, int(math.sqrt(self.num))):
+            if self.num % i == 0:
+                return 0
+        return 1
+
+    def get_prime(self):  # getting prime num from the inputting one
+        if self.num % 2 == 0:
+            self.num += 1
+        while not self.check_prime():
+            self.num += 2
+        return self.num
+
+    def hash_function(self, key):
+        capacity = self.get_prime()
+        return key % capacity
+
+    def insert_data(self, key, data):
+        index = self.hash_function(key)
+        self.table[index] = [key, data]
+
+    def remove_data(self, key):
+        index = self.table(key)
+        self.table[index] = 0
