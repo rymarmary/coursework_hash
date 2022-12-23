b25c8aeee0de7c8534390bf5d70288eb4f5acc2a
rymarmary <rymarmary2003@gmail.com>
2022-12-23 19:03:25 +0300

    Fixed structures && add test functions && report changes

--- hash_double.py
+++ hash_double.py
@@ -1,10 +1,13 @@
 import math
 
+
 class DoubleHash:
     def __init__(self, prime):
         self.num = prime
         self.get_prime()  # changing num
-        self.table = [[],] * self.num
+        self.table = [[], ] * self.num
+        self.k = 0.0  # fill factor
+        self.const = (math.sqrt(2) - 1) % 2  # const offset for hashing
 
     def check_prime(self):  # check if num is prime
         if self.num == 1 or self.num == 0:
@@ -25,10 +28,54 @@ class DoubleHash:
         capacity = self.get_prime()
         return key % capacity
 
-    def insert_data(self, key, data):
-        index = self.hash_function(key)
-        self.table[index] = [key, data]
+    def off_hash_function(self, key):
+        temp = int(self.num * ((self.const * key) - int(self.const * key)))
+        return temp
 
-    def remove_data(self, key):
-        index = self.table(key)
-        self.table[index] = 0
+    def insert_data(self, key, data, i=0):
+        if i < self.num and self.k < 2 / 3:
+            index = (self.hash_function(key) + i * self.off_hash_function(key)) % self.num
+            if self.table[index] and self.table[index] != ["delete"]:  # if cell is not empty
+                if self.table[index][0] == key:  # if key is exist in table
+                    self.table[index] = [key, data]  # update data
+                else:  # in case of collision
+                    i += 1
+                    self.insert_data(key, data, i)
+            else:
+                self.table[index] = [key, data]
+                self.k += 1 / self.num
+        else:  # resize to increase empty cells amount
+            saved_num = self.num
+            saved_table = self.table
+            self.num = self.num * 2
+            self.get_prime()
+            self.table = [[], ] * self.num
+            self.k = 0
+            for i in range(saved_num):
+                if saved_table[i]:
+                    self.insert_data(saved_table[i][0], saved_table[i][1], 0)
+            self.insert_data(key, data, 0)
+
+    def remove_data(self, key, i=0):
+        if i < self.num:
+            index = (self.hash_function(key) + i * self.off_hash_function(key)) % self.num
+            if self.table[index]:
+                if self.table[index][0] == key:
+                    self.table[index] = ["delete"]
+                else:
+                    i += 1
+                    self.remove_data(key, i)
+
+    def search_elem(self, key, i=0):
+        if i < self.num:
+            index = (self.hash_function(key) + i * self.off_hash_function(key)) % self.num
+            if self.table[index]:
+                if self.table[index][0] == key:
+                    return self.table[index][1]
+                else:
+                    i += 1
+                    return self.search_elem(key, i)
+            else:
+                return "didn't find"
+        else:
+            return "did't find"
---
+++ hash_double_test.py
@@ -0,0 +1,35 @@
+import hash_double
+
+hash_table = hash_double.DoubleHash(5)
+
+hash_table.insert_data(1, "one")  # testing insert function
+hash_table.insert_data(34, "thirty-four")
+hash_table.insert_data(17, "seventeen")
+
+print("insert: ", hash_table.table, "\n")
+
+hash_table.insert_data(90, "ninety")   # testing resize
+hash_table.insert_data(73, "seventy-three")
+
+print("resize: ", hash_table.table, "\n")
+
+hash_table.insert_data(55, "fifty")
+
+print("insert in resized table: ", hash_table.table, "\n")
+
+print("searching: ")
+
+print(hash_table.search_elem(34))  # searching existing element
+print(hash_table.search_elem(101))  # searching not existing element
+print(hash_table.search_elem(34 % 10))   # searching not existing element with existing hash
+
+hash_table.remove_data(34)  # trying to remove existing element
+hash_table.remove_data(101)  # trying to remove not existing element
+hash_table.remove_data(34 % 10)  # trying to remove not existing element with existing hash
+
+print()
+print("removing: ", hash_table.table, "\n")
+
+hash_table.remove_data(34)  # trying to remove already removed element
+
+print("removing already removed element: ", hash_table.table)
